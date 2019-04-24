c83a4a3764f667975fb387d35f9061f5205995b3
MinsuKim <kms040411@naver.com>
2019-04-25 01:00:46 +0900

    Implemented Mozaic menu, delete global vars

--- editor.py
+++ editor.py
@@ -1,43 +1,82 @@
 from tkinter import *
-from PIL import Image, ImageFilter
+from PIL import Image, ImageFilter, ImageTk
+from tkinter import filedialog
 
-def gui():
+import mozaic
+
+# global variables       
+undo_stack = None           # Undo Stack
+
+class App(Tk):
+    def __init__(self):
+        super().__init__()
+
+        # Init Program Variables
+        self.image = None               # Currently Open Image Object
+        self.photoimage = None          # Currently Open PhotoImage Object
+        self.file = None                # Currently Open File Path   
+
+        self.o_mozaic_window = False    # Is mozaic window opened?
+
+        self.undo_stack = list()        # Initialize Undo Stack
+
+        # Root Window
+        self.title("Simple Picture Editor")
+        self.geometry("640x400+100+100")
+        self.resizable(False, False)
+
+        # Widgets
+        self.canvas1 = Canvas(self)
+        self.canvas1.pack(fill="both")
+
+        # Events
+        self.bind_all('<Control-KeyPress-z>', self.undo)
+
+        # Menus
+        self.root_menu = Menu(self)
+        filemenu = Menu(self.root_menu, tearoff = 0)
+        filemenu.add_command(label = "Open", command = self.openfile)
+        filemenu.add_command(label = "Save", command = donothing)
+        filemenu.add_command(label = "Save as...", command = donothing)
+        filemenu.add_separator()
+        filemenu.add_command(label = "Exit", command = self.quit)
+        self.root_menu.add_cascade(label = "File", menu = filemenu)
+
+        editmenu = Menu(self.root_menu, tearoff = 0)
+        editmenu.add_command(label = "Mozaic", command = self.mozaic_menu)
+        self.root_menu.add_cascade(label = "Edit", menu = editmenu)
+
+        self.config(menu = self.root_menu)
+
+        self.mainloop()
+
+    def mozaic_menu(self):
+        print(self.o_mozaic_window)
+        if(self.o_mozaic_window):
+            return
+        self.o_mozaic_window = True
+        mozaic_window = mozaic.Mozaic_Window(self)
+
+    def openfile(self):
+        self.file = filedialog.askopenfilename(title = "Open File", filetypes = (("JPEG Image", "*.jfif"), ("png files", "*.png")))
+        self.image = Image.open(self.file)
+        self.refreshImage()
     
-    window = Tk()
-    window.title("Mozaik")
-    window.geometry("640x400+100+100")
-    window.resizable(False, False)
-    
-    
+    def refreshImage(self):
+        self.photoimage = ImageTk.PhotoImage(self.image)
+        current_width = self.photoimage.width()
+        current_height = self.photoimage.height()
+        root_width = self.winfo_x()
+        root_height = self.winfo_y()
 
-    window.mainloop()
+        self.geometry(str(current_width) + "x" + str(current_height) + "+" + str(root_width) + "+" + str(root_height))
+        self.canvas1.create_image(0, 0, image = self.photoimage, anchor = NW)
 
-def mozaik(image, level = 2):
-    pixels = image.load()
+    def undo(self, aux):
+        print("z pressed")
 
-    for i in range(0, image.size[0], level):
-        for j in range(0, image.size[1], level):
-            pixel_average(pixels, i, j, level, image.size)
-    
-    image.show()
-
-def pixel_average(pixels, left, top, level, size):
-    average = [0, 0, 0]
-    total_pixels = level * level
-    for i in range(left, min(left + level, size[0])):
-        for j in range(top, min(top + level, size[1])):
-            average[0] = average[0] + pixels[i, j][0]
-            average[1] = average[1] + pixels[i, j][1]
-            average[2] = average[2] + pixels[i, j][2]
-    average[0] = int(average[0] / total_pixels)
-    average[1] = int(average[1] / total_pixels)
-    average[2] = int(average[2] / total_pixels)
-
-    for i in range(left, min(left + level, size[0])):
-        for j in range(top, min(top + level, size[1])):
-            pixels[i, j] = tuple(average)
+def donothing():
+    pass    # Temp Function that does nothing
 
 if __name__ == "__main__":
-    #gui()
-    image = Image.open('ultimate.png')
-    mozaik(image, 30)
+    root = App()
---
+++ mozaic.py
@@ -0,0 +1,69 @@
+from tkinter import *
+from PIL import Image, ImageFilter, ImageTk
+from tkinter import filedialog
+
+import editor
+
+class Mozaic_Window(Toplevel):
+    def __init__(self, parent):
+        super(Mozaic_Window, self).__init__(parent)
+        self.title("Mozaic")
+        self.parent = parent
+        self.geometry("200x140+{}+{}".format(parent.winfo_x() + parent.winfo_width() + 30, parent.winfo_y()))
+        self.resizable(False, False)
+        self.drawWidgets()
+
+        self.target = 0     # 0: Entire, 1: Selected
+        self.level = 2      # Default mozaic level
+
+    def drawWidgets(self):
+        def radio():
+            self.target = targetOption.get()
+        targetOption = IntVar()
+        radio1 = Radiobutton(self, text = "Apply to the Entire Picture", value = 0, variable = targetOption, command = radio)
+        radio1.pack(side = "top")
+        radio2 = Radiobutton(self, text = "Apply to the Selected Area", value = 1, variable = targetOption, command = radio)
+        radio2.pack(side = "top")
+
+        def scale_func(aux):
+            self.level = mozaic_level.get()
+        mozaic_level = IntVar()
+        scale1 = Scale(self, label = "Set Mozaic Level", variable = mozaic_level, command = scale_func, orient = "horizontal", showvalue = True, resolution = 1, from_ = 2, to = 100)
+        scale1.pack(side = "top")
+
+        button1 = Button(self, text = "Apply", command = self.mozaic)
+        button1.pack(side = "top", fill = "x")
+        
+    def mozaic(self):
+        if(self.target == 0):
+            img = self.parent.image
+            if(img == None):
+                print("No Image")
+            pixels = img.load()
+            for i in range(0, img.size[0], self.level):
+                for j in range(0, img.size[1], self.level):
+                    self.pixel_average(pixels, i, j, self.level, img.size)
+            self.parent.refreshImage()
+        else:
+            return
+            
+    
+    def pixel_average(self, pixels, left, top, level, size):
+        average = [0, 0, 0]
+        total_pixels = level * level
+        for i in range(left, min(left + level, size[0])):
+            for j in range(top, min(top + level, size[1])):
+                average[0] = average[0] + pixels[i, j][0]
+                average[1] = average[1] + pixels[i, j][1]
+                average[2] = average[2] + pixels[i, j][2]
+        average[0] = int(average[0] / total_pixels)
+        average[1] = int(average[1] / total_pixels)
+        average[2] = int(average[2] / total_pixels)
+
+        for i in range(left, min(left + level, size[0])):
+            for j in range(top, min(top + level, size[1])):
+                pixels[i, j] = tuple(average)
+
+    def __del__(self):
+        self.parent.o_mozaic_window = False
+
